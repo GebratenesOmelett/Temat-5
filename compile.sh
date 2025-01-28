2277109c408fd606f99022334c69d273f16cdc91
Aleksy <78979897+GebratenesOmelett@users.noreply.github.com>
2025-01-28 14:02:56 +0100

    compiler update

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 gcc -o mainp mainp.c
 gcc -o pasazer pasazer.c -lpthread
-gcc -o lotnisko lotnisko.c
-gcc -o ksamolotu ksamolotu.c
-gcc -o dyspozytor dyspozytor.c
+gcc -o lotnisko lotnisko.c -lpthread
+gcc -o ksamolotu ksamolotu.c -lpthread
+gcc -o dyspozytor dyspozytor.c -lpthread
